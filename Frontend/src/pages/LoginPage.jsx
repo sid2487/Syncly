a99c3426137e5e808a8a0f152ffa655cceaf7a6d
sid2487 <sid454804@gmail.com>
2025-06-16 23:05:41 +0530

    fix some issues

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -43,7 +43,11 @@ const LoginPage = () => {
           {/* ERROR MESSAGE DISPLAY */}
           {error && (
             <div className="alert alert-error mb-4">
-              <span>{error.response.data.message}</span>
+              <span>
+                {error.response?.data?.message ||
+                  error.message ||
+                  "Something went wrong"}
+              </span>
             </div>
           )}
 
