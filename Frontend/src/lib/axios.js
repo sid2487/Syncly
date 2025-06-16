5adcf8b978c3547446e0d99c2f5107068327a424
sid2487 <sid454804@gmail.com>
2025-06-17 01:01:15 +0530

    did some changes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,17 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//     baseURL: "https://syncly-z1ur.onrender.com",
+//     withCredentials: true, 
+// })
+
 import axios from "axios";
 
+const baseURL = import.meta.env.PROD
+    ? "https://syncly-z1ur.onrender.com" // ⬅️ production backend
+    : import.meta.env.VITE_BACKEND_URL;   // ⬅️ development backend (localhost)
+
 export const axiosInstance = axios.create({
-    baseURL: "https://syncly-z1ur.onrender.com",
-    withCredentials: true, 
-})
+    baseURL,
+    withCredentials: true,
+});
